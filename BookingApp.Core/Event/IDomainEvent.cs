e5841cac60e61b4634810a9cb7b5b3f362ad6137
tiagoluis86 <tiagoluis86@outlook.com>
2022-12-25 19:24:45 -0300

    ADD Event, Exceptions

---
+++ BookingApp.Core/Event/IDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using BookingApp.Core.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Core.Event
+{
+    public interface IDomainEvent : IEvent
+    {
+
+    }
+}
---
+++ BookingApp.Core/Event/IEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BookingApp.Core.Event
+{
+    public interface IEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        public string? EventType => GetType().AssemblyQualifiedName;
+    }
+}
