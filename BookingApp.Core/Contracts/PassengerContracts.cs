a7153d3dd84d7275ca98e5330db05d9df6d954c5
tiagoluis86 <tiagoluis86@outlook.com>
2022-12-25 19:17:56 -0300

    ADD Contracts, EFCore

---
+++ BookingApp.Core/Contracts/PassengerContracts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Core.Contracts
+{
+    public record GetPassengerByIdRequest
+    {
+        public long PassengerId { get; init; }
+    }
+
+    public record PassengerResponse
+    {
+        public long Id { get; init; }
+        public string Name { get; init; }
+        public string PassportNumber { get; init; }
+        public PassengerType PassengerType { get; init; }
+        public int Age { get; init; }
+    }
+
+    public enum PassengerType
+    {
+        Male,
+        Female,
+        Baby,
+        Unknown
+    }
+
+    public record UserCreated(long Id, string Name, string PassportNumber);
+}
