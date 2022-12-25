72176dddd1a51c93b4b7d58282ff522d9f2c86fd
tiagoluis86 <tiagoluis86@outlook.com>
2022-12-25 19:40:17 -0300

    ADD Jwt, Generators

---
+++ BookingApp.Core/Generators/SnowFlakIdGenerator.cs
@@ -0,0 +1,29 @@
+﻿using IdGen;
+using System.Reflection.Emit;
+
+namespace BookingApp.Core.Generators
+{
+    public static class SnowFlakIdGenerator
+    {
+        private static IdGenerator _generator;
+
+        public static void Configure(int generatorId)
+        {
+
+            var epoch = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+
+            // Create an ID with 45 bits for timestamp, 2 for generator-id
+            // and 16 for sequence
+            var structure = new IdStructure(45, 2, 16);
+
+            // Prepare options
+            var options = new IdGeneratorOptions(structure, new DefaultTimeSource(epoch));
+
+            // Create an IdGenerator with it's generator-id set to 0, our custom epoch
+            // and id-structure
+            _generator = new IdGenerator(generatorId, options);
+        }
+
+        public static long NewId() => _generator.CreateId();
+    }
+}
