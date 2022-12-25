e2c58765e81004bfac8260dcdfd07df6958c49c5
tiagoluis86 <tiagoluis86@outlook.com>
2022-12-25 19:28:43 -0300

    ADD AppOptions, RabbitMqOptions

---
+++ BookingApp.Core/Options/AppOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Core.Options
+{
+    public class AppOptions
+    {
+        public string Name { get; set; }
+    }
+}
